7101571a907ee6c0ef23e6a68b98767a58db0336
Aashutosh <aashutoshparoha89@gmail.com>
2022-04-10 17:42:07 +0530

    1april

---
+++ April/1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n=s.size()-1;
+        int i=0;
+        while(i<n){
+            char c=s[i];
+            s[i]=s[n];s[n]=c;
+            i++;n--;
+        }
+    }
+};
