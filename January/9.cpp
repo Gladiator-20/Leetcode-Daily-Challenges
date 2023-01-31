ff59c0b5475f3aab63149f0ee36d30db0b5469cd
Aashutosh Paroha <84857062+Gladiator-20@users.noreply.github.com>
2023-01-31 18:29:21 +0000

    9

---
+++ January/9.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL){
+          return ans;  
+        }
+        ans.push_back(root->val);
+        vector<int>tem=preorderTraversal(root->left);
+        for(int it:tem)ans.push_back(it);
+        tem=preorderTraversal(root->right);
+        for(int it:tem)ans.push_back(it);
+        return ans;
+    }
+};
