4007dbec4a579e8143d1a23384cea47a40337298
Aashutosh Paroha <84857062+Gladiator-20@users.noreply.github.com>
2023-01-31 18:27:28 +0000

    6th

---
+++ January/6.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+	int maxIceCream(vector<int> &costs, int coins) {
+		sort(costs.begin(), costs.end());
+		int n = costs.size();
+		for (int i = 0; i < n; i++) {
+			if (costs[i] > coins)
+				return i;
+			else
+				coins -= costs[i];
+		}
+		return n;
+	}
+};
